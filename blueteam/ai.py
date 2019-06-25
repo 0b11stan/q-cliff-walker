a26076eadfb5d5216c6b631fea9a5c11190ff90e
ms-dev-pro <sebastien.marchal@protonmail.com>
2019-06-25 11:26:42 +0200

    working code with enum

--- blueteam/ai.py
+++ blueteam/ai.py
@@ -23,6 +23,18 @@
 import numpy as np
 import random
 
+from environment import Map, Environment
+
+landform_0 = [
+            [Map.LAND, Map.START, Map.LAND,   Map.DANGER],
+            [Map.LAND, Map.LAND,  Map.LAND,   Map.LAND],
+            [Map.LAND, Map.LAND,  Map.LAND, Map.LAND],
+            [Map.LAND, Map.LAND,  Map.LAND, Map.LAND],
+            [Map.LAND, Map.LAND,  Map.LAND,   Map.LAND],
+            [Map.LAND, Map.LAND,  Map.LAND,   Map.DANGER],
+            [Map.GOAL, Map.LAND,  Map.LAND,   Map.DANGER]
+        ]
+
 playground_0 = [
     ["START", "LAND", "LAND", "LAND"],
     ["DANGER", "DANGER", "DANGER", "LAND"],
@@ -73,25 +85,24 @@ def calculate_reward_matrix(playground):
         # iterate over columns
         for j in range(0, len(playground[i])):
             reward_row = []
-            input = playground[i][j]
             # UP
             if 0 <= i - 1 <= len(playground):
-                reward_row.append(get_reward_for_cell(playground[i - 1][j]))
+                reward_row.append(playground[i - 1][j].get_reward())
             else:
                 reward_row.append(0)
             # DOWN
             if 0 <= i + 1 < len(playground):
-                reward_row.append(get_reward_for_cell(playground[i + 1][j]))
+                reward_row.append(playground[i + 1][j].get_reward())
             else:
                 reward_row.append(0)
             # LEFT
             if 0 <= j - 1 < len(playground[i]):
-                reward_row.append(get_reward_for_cell(playground[i][j - 1]))
+                reward_row.append(playground[i][j - 1].get_reward())
             else:
                 reward_row.append(0)
             # RIGHT
             if 0 <= j + 1 < len(playground[i]):
-                reward_row.append(get_reward_for_cell(playground[i][j + 1]))
+                reward_row.append(playground[i][j + 1].get_reward())
             else:
                 reward_row.append(0)
             reward_matrix.append(reward_row)
@@ -105,7 +116,6 @@ def calculate_transition_matrix(playground):
         # iterate over columns
         for j in range(0, len(playground[i])):
             transition_row = []
-            input = playground[i][j]
             # UP
             if 0 <= i - 1 <= len(playground):
                 transition_row.append(((i - 1) * len(playground[i])) + j)
@@ -156,7 +166,7 @@ def get_lava_states_matrix(playground):
     lava_states_matrix = []
     for i in range(0, len(playground)):
         for j in range(0, len(playground[i])):
-            if playground[i][j] == "DANGER":
+            if playground[i][j] == Map.DANGER:
                 lava_states_matrix.append((i * len(playground[i])) + j)
     return lava_states_matrix
 
@@ -165,7 +175,7 @@ def get_goal_states_matrix(playground):
     goal_states_matrix = []
     for i in range(0, len(playground)):
         for j in range(0, len(playground[i])):
-            if playground[i][j] == "GOAL":
+            if playground[i][j] == Map.GOAL:
                 goal_states_matrix.append((i * len(playground[i])) + j)
     return goal_states_matrix
 
@@ -177,22 +187,40 @@ def is_lava_or_goal(state, lava_states_matrix, goal_states_matrix):
         return False
 
 
+def get_flat_matrix(matrix):
+    flat_matrix = []
+    for i in range(0, len(matrix)):
+        flat_matrix.append(matrix[i])
+    return flat_matrix
+
+def get_next_action(state, flat_reward_matrix, calculated_valid_actions_matrix, random_choice_rate=0.1):
+    if(np.random.random() < random_choice_rate):
+        return random.choice(calculated_valid_actions_matrix[state])
+    else:
+        index = flat_reward_matrix[state].index(max(flat_reward_matrix[state]))
+        if(index in calculated_valid_actions_matrix[state]):
+            return index
+        else:
+            return random.choice(calculated_valid_actions_matrix[state])
 
 
-calculated_reward_matrix = calculate_reward_matrix(playground_0)
-calculated_transition_matrix = calculate_transition_matrix(playground_0)
+
+
+
+calculated_reward_matrix = calculate_reward_matrix(landform_0)
+calculated_flat_reward_matrix = get_flat_matrix(calculated_reward_matrix)
+calculated_transition_matrix = calculate_transition_matrix(landform_0)
 calculated_valid_actions_matrix = calculate_valid_actions_matrix(calculated_transition_matrix)
-calculated_lava_states_matrix = get_lava_states_matrix(playground_0)
-calculated_goal_states_matrix = get_goal_states_matrix(playground_0)
+calculated_lava_states_matrix = get_lava_states_matrix(landform_0)
+calculated_goal_states_matrix = get_goal_states_matrix(landform_0)
 
 print_matrix(calculated_reward_matrix, "REWARD")
 print_matrix(calculated_transition_matrix, "TRANSITION")
 print_matrix(calculated_valid_actions_matrix, "VALID ACTIONS")
-
 # replace 32 by number of cells
 # 4 is the number of possible states
 def launch_qlearning(calculated_reward_matrix, calculated_transition_matrix, calculated_valid_actions_matrix):
-    q_matrix = np.zeros((32, 4))
+    q_matrix = np.zeros((28, 4))
 
     gamma = 0.8
 
@@ -200,11 +228,12 @@ def launch_qlearning(calculated_reward_matrix, calculated_transition_matrix, cal
 
     for i in range(episodes):
         # replace starting state depending on playground (TODO code function to automate it)
-        start_state = 24
+        start_state = 1
         current_state = start_state
         # While diamond not found
         while not is_lava_or_goal(current_state, calculated_lava_states_matrix, calculated_goal_states_matrix):
-            action = random.choice(calculated_valid_actions_matrix[current_state])
+            action = get_next_action(current_state, calculated_flat_reward_matrix, calculated_valid_actions_matrix)
+            #action = random.choice(calculated_valid_actions_matrix[current_state])
             next_state = calculated_transition_matrix[current_state][action]
             future_rewards = []
             for next_action in calculated_valid_actions_matrix[next_state]:
@@ -218,4 +247,4 @@ def launch_qlearning(calculated_reward_matrix, calculated_transition_matrix, cal
     print(q_matrix)
 
 
-#launch_qlearning(calculated_reward_matrix, calculated_transition_matrix, calculated_valid_actions_matrix)
+launch_qlearning(calculated_reward_matrix, calculated_transition_matrix, calculated_valid_actions_matrix)
